af95f95086daa5573ea28aedb35b384f1adb26a5
Ubuntu_16.04 <mark.lizo@fh-bielefeld.de>
2021-01-20 16:26:19 +0100

    client.C

--- client.C
+++ client.C
@@ -7,9 +7,11 @@
 
 #include <string>
 #include <iostream>
+#include <cstdio>
 #include <unistd.h> //contains various constants
 #include "SIMPLESOCKET.H"
 #include "TASK1.H"
+#include <ctime>
 using namespace TASK1;
 using namespace std;
 
@@ -24,10 +26,14 @@ using namespace std;
  *
  * */
 
-void PWDRaten(string const& chars, size_t max_len, string const& cur){
-	string meinpwd ="abcd";
+int PWDRaten(string const& chars, size_t max_len, string const& cur){
+	string meinpwd ="Bbcd";
+	clock_t time;
+	double duration;
+
+	time = clock();
 	if (cur.length()== max_len){
-		return;
+		return 0;
 	}
 	else {
 		for (auto c : chars){
@@ -35,7 +41,10 @@ void PWDRaten(string const& chars, size_t max_len, string const& cur){
 			cout << next << endl;
 			if(next==meinpwd){
 				cout<<"Password geknackt"<<endl;
+				duration = (clock() - time) / (double) CLOCKS_PER_SEC;
+				cout << " Das erraten des Password hat"<< duration << " sekunden gedauert."<<endl;
 				exit(1);
+
 			}
 			else{
 				PWDRaten(chars,max_len,next);
@@ -53,7 +62,7 @@ int main() {
 	TCPclient c;
 	string host = "localhost";
 	string msg;
-
+	string newmsg;
 	//connect to host
 	c.conn(host , 2024);
 
@@ -61,7 +70,7 @@ int main() {
 
 
 	string symbols=TASK1::SYMBOLS;
-	PWDRaten(symbols,4,"");
+	newmsg=PWDRaten(symbols,4,"");
 
 	bool goOn=1;
 	while(goOn){ // send and receive data
@@ -71,10 +80,10 @@ int main() {
 		}else{
 			msg = string("client wants this");
 		}
-		cout << "client sends:" << msg << endl;
-		c.sendData(msg);
+		cout << "client sends:" << newmsg << endl;
+		c.sendData(newmsg);
 		msg = c.receive(32);
-		cout << "got response:" << msg << endl;
+		cout << "got response:" << newmsg << endl;
 		sleep(1);
 
 
